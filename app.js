c16a20655e8060a0fdb2e93009bbed3c1b5d90f1
Vladislav001 <VladGurjev@mail.ru>
2018-10-17 19:01:14 +0300

    Добавил немного маршуртизации

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var flash = require('connect-flash');
 var app = express();
 
+// use ejs-locals for all ejs templates:
+app.engine('ejs', require('ejs-locals'));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'template'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public/systemImages', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+var routes = require('./routes/index');
+app.use(flash());
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// module.exports = function(){
+//
+//   router.get('/', require('./main').get);
+//
+//   return router;
+// }
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('main');
 });
 
 module.exports = router;
