52f8e8c0d36abe19950446cdde741980afa66806
Vladislav001 <VladGurjev@mail.ru>
2018-10-17 22:05:38 +0300

    правки

--- routes/elevation.js
+++ routes/elevation.js
@@ -1,4 +1,4 @@
-const  axios = require('axios');
+const axios = require('axios');
 
 exports.post = function(req, res) {
   var latitude = req.body.latitude;
@@ -6,17 +6,17 @@ exports.post = function(req, res) {
   // https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933
   var url = 'https://api.open-elevation.com/api/v1/lookup?locations=' + latitude + ',' + longitude;
 
-  const getElevation = async url => {
-    try {
-      const response = await axios.get(url);
-      const data = response.data;
-
-      res.send(JSON.stringify(data.results[0].elevation));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  getElevation(url);
+  // const getElevation = async url => {
+  //   try {
+  //     const response = await axios.get(url);
+  //     const data = response.data;
+  //
+  //     res.send(JSON.stringify(data.results[0].elevation));
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  //
+  // getElevation(url);
 
 }
