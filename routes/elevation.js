0ffda443e4154dcc5520129422f1a42d7a196bb6
Vladislav001 <VladGurjev@mail.ru>
2018-10-17 22:08:35 +0300

    правки

--- routes/elevation.js
+++ routes/elevation.js
@@ -1,22 +1,22 @@
 const axios = require('axios');
 
-exports.post = function(req, res) {
-  // var latitude = req.body.latitude;
-  // var longitude = req.body.longitude;
-  // // https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933
-  // var url = 'https://api.open-elevation.com/api/v1/lookup?locations=' + latitude + ',' + longitude;
-  //
-  // const getElevation = async url => {
-  //   try {
-  //     const response = await axios.get(url);
-  //     const data = response.data;
-  //
-  //     res.send(JSON.stringify(data.results[0].elevation));
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  //
-  // getElevation(url);
-
-}
+// exports.post = function(req, res) {
+//   var latitude = req.body.latitude;
+//   var longitude = req.body.longitude;
+//   // https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933
+//   var url = 'https://api.open-elevation.com/api/v1/lookup?locations=' + latitude + ',' + longitude;
+//
+//   const getElevation = async url => {
+//     try {
+//       const response = await axios.get(url);
+//       const data = response.data;
+//
+//       res.send(JSON.stringify(data.results[0].elevation));
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+//
+//   getElevation(url);
+//
+// }
