233ef2371ce0609238b57deba1c5519559e177b9
Vladislav001 <VladGurjev@mail.ru>
2018-10-17 21:14:46 +0300

    Вычиcление высоты

--- routes/elevation.js
+++ routes/elevation.js
@@ -1,39 +1,25 @@
 const  axios = require('axios');
-const https = require('https');
-
-// exports.get = async function(req, res) {
-//     console.log('elev');
-//      var url = 'https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933';
-//   //   https.get(url,(res)=>{
-//   //   let body = '';
-//   //   res.on('data',(elem) => body +=elem);
-//   //   res.on('end',()=> console.log(JSON.parse(body)));
-//   // });
-//
-//   const getLocation = async url => {
-//     try {
-//       const response = await axios.get(url);
-//       const data = response.data;
-//       console.log(
-//         data
-//       );
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//
-//   getLocation(url);
-//
-//
-// }
 
 exports.post = function(req, res) {
   var latitude = req.body.latitude;
   var longitude = req.body.longitude;
   var elevation;
 
-  console.log(latitude + ' ' + longitude);
-elevation = '213';
+  // https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933
+  var url = 'https://api.open-elevation.com/api/v1/lookup?locations=' + latitude + ',' + longitude;
 
-   res.send(JSON.stringify(elevation));
+  const getLocation = async url => {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+
+      res.send(JSON.stringify(data.results[0].elevation));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getLocation(url);
+
 }
+ 
