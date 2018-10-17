d8bc3514cd089c2683e292bbc64d82f4fbf56f76
Vladislav001 <VladGurjev@mail.ru>
2018-10-18 00:58:06 +0300

    прав

--- routes/elevation.js
+++ routes/elevation.js
@@ -5,18 +5,18 @@ exports.post = function(req, res) {
   var longitude = req.body.longitude;
   // https://api.open-elevation.com/api/v1/lookup?locations=41.161758,-8.583933
   var url = 'https://api.open-elevation.com/api/v1/lookup?locations=' + latitude + ',' + longitude;
-  
-  // const getElevation = async url => {
-  //   try {
-  //     const response = await axios.get(url);
-  //     const data = response.data;
-  //
-  //     res.send(JSON.stringify(data.results[0].elevation));
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  //
-  // getElevation(url);
+
+  const getElevation = async url => {
+    try {
+      const response = await axios.get(url);
+      const data = response.data;
+
+      res.send(JSON.stringify(data.results[0].elevation));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getElevation(url);
 
 };
