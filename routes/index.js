d432e60a3e71659878bd45d188ac3d6480b0d33d
Vladislav001 <VladGurjev@mail.ru>
2018-10-17 21:58:05 +0300

    api elevation

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ var router = express.Router();
 router.get('/', require('./main').get);
 router.post('/elevation', require('./elevation').post);
 
+// API
+router.post('/api/v1/elevation', require('./api/v1/elevation').post);
+
+
 module.exports = router;
