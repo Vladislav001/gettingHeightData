e307d9afd2ff79a3537f3f3436bf915b2b6298ae
Vladislav001 <VladGurjev@mail.ru>
2018-10-18 00:36:22 +0300

    правка

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 
 router.get('/', require('./main').get);
-//router.post('/elevation', require('./elevation').post);
-router.post('/addNewVideo', require('./addVideo').post);
+router.post('/elevation', require('./elevation').post);
 
 // API
 //router.post('/api/v1/elevation', require('./api/v1/elevation').post);
