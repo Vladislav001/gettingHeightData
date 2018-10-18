6cc55ab5ce1374c05b6dff4678afb6c5f9fe8d19
Vladislav001 <VladGurjev@mail.ru>
2018-10-18 19:25:56 +0300

    правки апи

--- routes/api/v1/elevation.js
+++ routes/api/v1/elevation.js
@@ -3,30 +3,25 @@ const  axios = require('axios');
 exports.post = function(req, res) {
   var latitude = req.body.latitude;
   var longitude = req.body.longitude;
-  var url = 'https://api.open-elevation.com/api/v1/lookup?locations=' + latitude + ',' + longitude;
+  //var url = 'https://api.open-elevation.com/api/v1/lookup?locations=' + latitude + ',' + longitude;
+  var url2 = 'https://elevation-api.io/api/elevation?points=(' + latitude + ',' + longitude + ')';
 
   const getElevation = async url => {
     try {
       const response = await axios.get(url);
       const data = response.data;
 
-      if(typeof data.results[0].elevation !=="undefined")
-      {
-        res.status(200).send({
-          elevation: data.results[0].elevation,
-          latitude: latitude,
-          longitude: longitude,
-         });
-      } else {
-        res.status(200).send({
-          error: "Не удалось загрузить данные, попробуйте снова"
-         });
-      }
+      res.status(200).send({
+        //  elevation: data.results[0].elevation,
+        elevation: data.elevations[0].elevation,
+        latitude: latitude,
+        longitude: longitude,
+      });
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  getElevation(url);
+getElevation(url2);
 };
